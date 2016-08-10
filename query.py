8dfb8f77df7363e832693d52244c4d85b83c7217
Ling <lingsitu1290@gmail.com>
2016-08-10 06:47:51 +0000

    Corrected errors.

--- query.py
+++ query.py
@@ -23,14 +23,14 @@ init_app()
 Brand.query.get(8)
 
 # Get all models with the **name** Corvette and the **brand_name** Chevrolet.
-Model.query.filter_by(name=='Corvette', brand_name=='Chevrolet').all()
+Model.query.filter_by(name='Corvette', brand_name='Chevrolet').all()
 
 # Get all models that are older than 1960.
 # Why can't I use d filter_by(year <1960)?
 Model.query.filter(Model.year < 1960).all()
 
 # Get all brands that were founded after 1920.
-Brand.query.filter(Brand.founded < 1920).all()
+Brand.query.filter(Brand.founded > 1920).all()
 
 # Get all models with names that begin with "Cor".
 Model.query.filter(Model.name.like('Cor%')).all()
@@ -40,7 +40,7 @@ Brand.query.filter(Brand.founded == 1903, Brand.discontinued.is_(None)).all()
 
 # Get all brands that are either 1) discontinued (at any time) or 2) founded 
 # before 1950.
-Brand.query.filter(Brand.discontinued.is_(None), Brand.founded < 1950).all()
+Brand.query.filter(Brand.discontinued.isnot(None) | Brand.founded < 1950).all()
 
 # Get any model whose brand_name is not Chevrolet.
 Model.query.filter(Model.brand_name != 'Chevrolet').all()
@@ -52,13 +52,18 @@ def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
 
-    db.session.query(Model.name, Model.brand_name, Brand.headquarters).filter(Model.year==year).all()
+    list_of_cars = db.session.query(Model.name, Model.brand_name, Brand.headquarters).filter(Model.year==year).all()
+
+    # unpack tuples 
+    for model_name, model_brand_name, brand_headquarters in list_of_cars:
+        print model_name, model_brand_name, brand_headquarters
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
      using only ONE database query.'''
 
-    db.session.query(Model.brand_name, Model.name).all()
+    return db.session.query(Model.brand_name, Model.name).all()
+
 
 # -------------------------------------------------------------------
 Part 2.5: Discussion Questions (Include your answers as comments.)
@@ -76,11 +81,11 @@ to connect on. Associate tables manages many to many relationships.
 def search_brands_by_name(mystr):
     """Return list of brand objects whose name contains or equal to the input string."""
 
-    Brand.query.filter(Brand.name.like(mystr)).all()
+    return Brand.query.filter(Brand.name.like('%mystr%')).all()
 
 def get_models_between(start_year, end_year):
     """Returns a list of model ojects with years that falls between 
     the start year(inclusive) and end year(exclusive).
     """
 
-    Model.query.filter(Model.year >= start_year, Model.year < end_year).all()
+    return Model.query.filter(Model.year >= start_year, Model.year < end_year).all()
